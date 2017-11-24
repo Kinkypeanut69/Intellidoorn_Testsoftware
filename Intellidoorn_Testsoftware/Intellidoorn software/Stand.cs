93e6d87afa2948ac340b5f7ef36767096ff961cf
Kinkypeanut69 <balintharmse24@gmail.com>
2017-11-24 10:04:27 +0100

    Removed gitignore

---
+++ Intellidoorn_Testsoftware/Intellidoorn software/Stand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntellidoornClient
+{
+    class Stand
+    {
+        public int locationId { get; private set; }
+        public string tagId { get; private set; }
+        public string locationDescription { get; private set; }
+        public bool isDock { get; private set; }
+
+        public Stand(int locationId, string tagId, string description, bool isDock)
+        {
+            this.locationId = locationId;
+            this.tagId = tagId;
+            locationDescription = description;
+            this.isDock = isDock;
+        }
+    }
+}
