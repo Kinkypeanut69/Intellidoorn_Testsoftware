0c1c55fce3b57eec8609152ff93cffccb3bbf47e
Sybercore <S1091072@student.windesheim.nl>
2017-11-29 15:34:45 +0100

    Adding SerialCommunication
    SerialCommunication Class

---
+++ Intellidoorn_Testsoftware/Intellidoorn software/Serial.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+
+
+
+namespace SerialConsole
+{
+    class Serial
+    {
+        private System.IO.Ports.SerialPort serialPort1;
+
+        public Serial()
+        {
+
+
+            this.serialPort1 = new SerialPort();
+            serialPort1.PortName = "COM3";
+            serialPort1.BaudRate = 19200;
+        }
+
+        public double ReadData()
+        {
+            double Output = 0;
+            serialPort1.Open();
+
+            serialPort1.WriteLine("F");
+            try
+            {
+                string input = serialPort1.ReadLine();
+                int index = input.IndexOf("m");
+                if (index > 0)
+                    input = input.Substring(0, index);
+
+                index = input.LastIndexOf(":") + 1;
+                input = (input.Substring(index, input.Length - index));
+
+                Output = Double.Parse(input);
+
+            }
+            catch (Exception ex)
+            {
+
+
+                Console.WriteLine(ex);
+            }
+
+            serialPort1.Close();
+            return Output;
+        }
+    }
+}
